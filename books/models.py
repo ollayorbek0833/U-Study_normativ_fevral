46c90697121907c71dd521394f22c244a29d4ddc
Ollayorbek <ollayorbek2005@gmail.com>
2026-02-21 17:28:33 +0500

    3-normativ: Soft delete with Custom QuerySet

--- books/models.py
+++ books/models.py
@@ -9,3 +9,34 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class BaseQuerySet(models.QuerySet):
+    def delete(self):
+        count = self.update(is_deleted = True)
+        model_label = f"{self.model._meta.app_label}.{self.model.__name__}"
+        return count, {model_label: count}
+    
+
+class DeletedManager(models.Manager):
+    def get_queryset(self):
+        return BaseQuerySet(self.model, using=self._db).filter(is_deleted = False)
+    
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(default=False)
+
+    objects = DeletedManager()
+    all_objects = BaseQuerySet.as_manager()
+
+    def __str__(self):
+        return self.title
+    
+    def delete(self, using = None, keep_paren = False):
+        self.is_deleted = True
+        self.save(update_fields=["is_deleted", "updated_at"])
+        model_label = f"{self._meta.app_label}.{self.__class__.__name__}"
+        return 1, {model_label:1}
